875e7c80c46dd3625c3ab1ffaf080b069b0975e8
davifeliciano <dfeliciano37@gmail.com>
2021-09-22 14:54:03 -0300

    Correcting the angles and add stderr to exported data

--- spectrum.py
+++ spectrum.py
@@ -8,7 +8,8 @@ from scipy.ndimage import gaussian_filter1d
 from scipy.signal import find_peaks
 
 LIGHT_SPEED = 299792458
-CRYSTAL = 201.4e-12
+CRYSTAL_LIF = 201.4e-12
+CRYSTAL_KBR = 329.5e-12
 PLANCK_JS = 6.62607004e-34
 PLANCK_EV = 4.13566769e-15
 RYDBERG = 3.28989e15
@@ -28,6 +29,7 @@ def get_filename(path):
     return path.split("\\")[-1].split("/")[-1]
 
 
+# Importing files and sorting data
 files = glob.glob("data/data1/Tubo*.dat")
 crystals = [get_filename(file).split(" ")[4] for file in files]
 apertures = [
@@ -45,11 +47,15 @@ fig, axes = plt.subplots(3, 1)
 
 axes[-1].set(xlabel=r"$\theta$ (\si{\degree})")
 
+# Params to use in find_peaks for each dataset
 fp_params = (
     {"height": None, "threshold": 0.3, "width": 5},
     {"height": 30.0, "threshold": None, "width": None},
     {"height": None, "threshold": 2.0, "width": None},
 )
+
+# Adding correction to the angles based on the result of angular.py
+correction_df = pd.read_csv("results/angular_result.csv", decimal=",")
 
 for ax, crystal, aperture, voltage, df, params in zip(
     axes, crystals, apertures, voltages, dfs, fp_params
@@ -60,7 +66,14 @@ for ax, crystal, aperture, voltage, df, params in zip(
         ylabel="Impulsos",
     )
 
-    x_data = gaussian_filter1d(df.loc[:, "Angle"], sigma=2)
+    if aperture == 2.0:
+        correction = correction_df.loc[0, "center"]
+        angle_stderr = correction_df.loc[0, "center_stderr"]
+    if aperture == 5.0:
+        correction = correction_df.loc[1, "center"]
+        angle_stderr = correction_df.loc[1, "center_stderr"]
+
+    x_data = gaussian_filter1d(df.loc[:, "Angle"], sigma=2) + correction
     y_data = gaussian_filter1d(df.loc[:, "Impulses"], sigma=2)
 
     ax.plot(x_data, y_data)
@@ -79,25 +92,6 @@ for ax, crystal, aperture, voltage, df, params in zip(
     k_beta_indexes = [peak for i, peak in enumerate(peak_indexes) if i % 2 == 0]
     k_alpha_angles = x_data[k_alpha_indexes]
     k_beta_angles = x_data[k_beta_indexes]
-    one_over_sin_k_alpha = 1 / np.sin(np.radians(k_alpha_angles))
-    one_over_sin_k_beta = 1 / np.sin(np.radians(k_beta_angles))
-
-    k_alpha_energies_js = [
-        0.5 * PLANCK_JS * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
-        for i, one_over_sin in enumerate(one_over_sin_k_alpha)
-    ]
-    k_alpha_energies_kev = [
-        5e-4 * PLANCK_EV * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
-        for i, one_over_sin in enumerate(one_over_sin_k_alpha)
-    ]
-    k_beta_energies_js = [
-        0.5 * PLANCK_JS * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
-        for i, one_over_sin in enumerate(one_over_sin_k_beta)
-    ]
-    k_beta_energies_kev = [
-        5e-4 * PLANCK_EV * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
-        for i, one_over_sin in enumerate(one_over_sin_k_beta)
-    ]
 
     ax.vlines(
         k_alpha_angles,
@@ -117,28 +111,117 @@ for ax, crystal, aperture, voltage, df, params in zip(
         label=r"$K_{\beta}$",
     )
 
+    # Computing physical variables of interest
+    k_alpha_angles_stderr = np.full(k_alpha_angles.size, angle_stderr)
+    k_beta_angles_stderr = np.full(k_beta_angles.size, angle_stderr)
+    k_alpha_bragg = k_alpha_angles / 2
+    k_beta_bragg = k_beta_angles / 2
+    k_alpha_bragg_stderr = k_alpha_angles_stderr / 2
+    k_beta_bragg_stderr = k_beta_angles_stderr / 2
+    k_alpha_bragg_rad = np.radians(k_alpha_bragg)
+    k_beta_bragg_rad = np.radians(k_beta_bragg)
+    k_alpha_bragg_rad_stderr = np.radians(k_alpha_bragg_stderr)
+    k_beta_bragg_rad_stderr = np.radians(k_beta_bragg_stderr)
+
+    one_over_sin_k_alpha = 1 / np.sin(k_alpha_bragg_rad)
+    one_over_sin_k_beta = 1 / np.sin(k_beta_bragg_rad)
+
+    one_over_sin_k_alpha_stderr = (
+        k_alpha_bragg_rad_stderr
+        * np.cos(k_alpha_bragg_rad)
+        / np.sin(k_alpha_bragg_rad) ** 2
+    )
+    one_over_sin_k_beta_stderr = (
+        k_beta_bragg_rad_stderr
+        * np.cos(k_beta_bragg_rad)
+        / np.sin(k_beta_bragg_rad) ** 2
+    )
+
+    if crystal == "LiF":
+        CRYSTAL = CRYSTAL_LIF
+    if crystal == "KBr":
+        CRYSTAL = CRYSTAL_KBR
+
+    k_alpha_energies_js = [
+        0.5 * PLANCK_JS * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
+        for i, one_over_sin in enumerate(one_over_sin_k_alpha)
+    ]
+    k_alpha_energies_js_stderr = [
+        0.5 * PLANCK_JS * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
+        for i, one_over_sin in enumerate(one_over_sin_k_alpha_stderr)
+    ]
+
+    k_alpha_energies_kev = [
+        5e-4 * PLANCK_EV * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
+        for i, one_over_sin in enumerate(one_over_sin_k_alpha)
+    ]
+    k_alpha_energies_kev_stderr = [
+        5e-4 * PLANCK_EV * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
+        for i, one_over_sin in enumerate(one_over_sin_k_alpha_stderr)
+    ]
+
+    k_beta_energies_js = [
+        0.5 * PLANCK_JS * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
+        for i, one_over_sin in enumerate(one_over_sin_k_beta)
+    ]
+    k_beta_energies_js_stderr = [
+        0.5 * PLANCK_JS * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
+        for i, one_over_sin in enumerate(one_over_sin_k_beta_stderr)
+    ]
+
+    k_beta_energies_kev = [
+        5e-4 * PLANCK_EV * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
+        for i, one_over_sin in enumerate(one_over_sin_k_beta)
+    ]
+    k_beta_energies_kev_stderr = [
+        5e-4 * PLANCK_EV * LIGHT_SPEED * one_over_sin / ((i + 1) * CRYSTAL)
+        for i, one_over_sin in enumerate(one_over_sin_k_beta_stderr)
+    ]
+
     ax.set(ylim=ylim)
     ax.legend(loc="upper left")
 
+    # Puting data of interest in a DataFrame to export as a csv
     result_df = pd.DataFrame(
         [
             k_beta_angles,
+            k_beta_angles_stderr,
+            k_beta_bragg,
+            k_beta_bragg_stderr,
             k_beta_energies_js,
+            k_beta_energies_js_stderr,
             k_beta_energies_kev,
+            k_beta_energies_kev_stderr,
             k_alpha_angles,
+            k_alpha_angles_stderr,
+            k_alpha_bragg,
+            k_alpha_bragg_stderr,
             k_alpha_energies_js,
+            k_alpha_energies_js_stderr,
             k_alpha_energies_kev,
+            k_alpha_energies_kev_stderr,
         ],
         index=(
             "k_beta_angles",
+            "k_beta_angles_stderr",
+            "k_beta_bragg",
+            "k_beta_bragg_stderr",
             "k_beta_energies_js",
+            "k_beta_energies_js_stderr",
             "k_beta_energies_kev",
+            "k_beta_energies_kev_stderr",
             "k_alpha_angles",
+            "k_alpha_angles_stderr",
+            "k_alpha_bragg",
+            "k_alpha_bragg_stderr",
             "k_alpha_energies_js",
+            "k_alpha_energies_js_stderr",
             "k_alpha_energies_kev",
+            "k_alpha_energies_kev_stderr",
         ),
     )
 
+    # Exporting it to results directory
     result_df.transpose().to_csv(
         f"results/spectre_{crystal}_{aperture}mm_{voltage}kV.csv",
         decimal=",",
